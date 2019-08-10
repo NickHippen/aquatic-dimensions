a6516e170924bd0bf98e541b9588aeeebf9e33fe
NickHippen <nhippen973@gmail.com>
2019-08-09 23:06:51 -0500

    Created empty plugin

---
+++ src/main/java/net/infernalrealms/aquatic_dimensions/AquaticDimensionsPlugin.java
@@ -0,0 +1,15 @@
+package net.infernalrealms.aquatic_dimensions;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class AquaticDimensionsPlugin extends JavaPlugin {
+
+	@Override
+	public void onEnable() {
+	}
+
+	@Override
+	public void onDisable() {
+	}
+
+}
