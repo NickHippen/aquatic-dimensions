6ad0896c09aa23d77f7312e8a9376648d603a0e4
Nick Hippen <nhippen973@gmail.com>
2019-08-22 16:28:30 -0500

    Removed old init function in ADArmorStand

--- src/main/java/net/infernalrealms/aquatic_dimensions/mobs/types/ADArmorStand.java
+++ src/main/java/net/infernalrealms/aquatic_dimensions/mobs/types/ADArmorStand.java
@@ -2,11 +2,6 @@ package net.infernalrealms.aquatic_dimensions.mobs.types;
 
 import java.lang.reflect.Field;
 
-import org.bukkit.Material;
-import org.bukkit.entity.LivingEntity;
-import org.bukkit.inventory.ItemStack;
-
-import net.infernalrealms.aquatic_dimensions.mobs.MonsterData;
 import net.minecraft.server.v1_14_R1.EntityTypes;
 import net.minecraft.server.v1_14_R1.World;
 
@@ -21,13 +16,6 @@ public class ADArmorStand extends ADMonster {
 		} catch (NoSuchFieldException | SecurityException | IllegalArgumentException | IllegalAccessException e) {
 			e.printStackTrace();
 		}
-	}
-	
-	@Override
-	public void init(MonsterData mobData) {
-		super.init(mobData);
-		((LivingEntity) getBukkitEntity()).getEquipment().setItemInMainHand(new ItemStack(Material.DIAMOND_AXE));
-		((LivingEntity) getBukkitEntity()).getEquipment().setChestplate(new ItemStack(Material.DIAMOND_CHESTPLATE));
 	}
 	
 }
